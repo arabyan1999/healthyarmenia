ae99466f93e0f1b4c86b17ff27f8ee71d771d7b0
Davit2123 <arabyandavit777@gmail.com>
2024-05-22 20:41:20 +0400

    alternative langdropdown

--- src/components/nav/LanguageDropdown/index.jsx
+++ src/components/nav/LanguageDropdown/index.jsx
@@ -1,26 +1,62 @@
 import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { StyledLanguageFropdown } from './styled';
+// import { StyledLanguageFropdown } from './styled';
 
-function LanguageDropdown() {
-  const [selectedLanguage, setSelectedLanguage] = useState('am');
+// function LanguageDropdown() {
+//   const [selectedLanguage, setSelectedLanguage] = useState('am');
 
-  const { t, i18n } = useTranslation();
+//   const { t, i18n } = useTranslation();
 
-  const handleLanguageChange = (e) => {
-    setSelectedLanguage(e.target.value);
-    i18n.changeLanguage(e.target.value);
+//   const handleLanguageChange = (e) => {
+//     setSelectedLanguage(e.target.value);
+//     i18n.changeLanguage(e.target.value);
+//   };
+
+//   return (
+//     <StyledLanguageFropdown>
+//       <select id="language-select" value={selectedLanguage} onChange={(e) => handleLanguageChange(e)}>
+//         <option value="am">AM</option>
+//         <option value="ru">RU</option>
+//         <option value="en">EN</option>
+//       </select>
+//     </StyledLanguageFropdown>
+//   );
+// }
+
+// export default LanguageDropdown;
+
+import './dropdown.css'; // Import your CSS file for styling
+
+const Dropdown = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedLang, setSelectedLang] = useState("ՀԱՅ");
+  const { i18n } = useTranslation();
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
   };
 
-  return (
-    <StyledLanguageFropdown>
-      <select id="language-select" value={selectedLanguage} onChange={(e) => handleLanguageChange(e)}>
-        <option value="am">AM</option>
-        <option value="ru">RU</option>
-        <option value="en">EN</option>
-      </select>
-    </StyledLanguageFropdown>
-  );
-}
+  const changeLanguage = (item) => {
+    setSelectedLang(item.title);
+    i18n.changeLanguage(item.value);
+  }
 
-export default LanguageDropdown;
+  const items = [{title: "ՀԱՅ", value: "am"}, {title: "ENG", value: "en"}, {title: "РУС", value: "ru"}]
+
+  return (
+    <div className="dropdown" onMouseEnter={toggleDropdown} onMouseLeave={toggleDropdown}>
+      <div className="dropdown-toggle">
+        {selectedLang}
+      </div>
+      {isOpen && (
+        <ul className="dropdown-menu">
+          {items.filter(el => el.title !== selectedLang).map((item, index) => (
+            <li key={index} onClick={() => {changeLanguage(item)}}>{item.title}</li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default Dropdown;
