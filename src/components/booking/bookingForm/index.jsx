7f38301f82277c163c693e440be19820c7361a7b
Davit2123 <arabyandavit777@gmail.com>
2024-05-23 23:30:58 +0400

    some changes

--- src/components/booking/bookingForm/index.jsx
+++ src/components/booking/bookingForm/index.jsx
@@ -1,48 +1,90 @@
 import React, { useState } from "react";
-import { StyledBookingForm, StyledFlex, StyledForm, StyledInput, StyledSubmitButton, StyledTitle } from "./styled";
+import { useTranslation } from "react-i18next";
+import { SelectItems, StyledBlockFlex, StyledBookingForm, StyledCustomSelect, StyledFlex, StyledForm, StyledInput, StyledOption, StyledSelect, StyledSelected, StyledSubmitButton, StyledTextarea, StyledTitle } from "./styled";
 
 function BookingForm() {
     const [data, setData] = useState({});
+    const [open, setOpen] = useState(false);
+    const { t } = useTranslation();
+
+    const typeOfService = [t("narrow_specialist_advice"), t("general_advice_consultation"), t("prophylactic_examination"),]
 
     const setNewData = (prevState, objectKey, objectValue) => {
         let newData = Object.assign({}, prevState);
         newData[objectKey] = objectValue;
+        console.log(newData);
         return newData;
     }
 
     return (
         <StyledBookingForm>
             <StyledTitle>
-                Պատվիրել հետադարձ կապ
+                {t("book_call")}
             </StyledTitle>
             <StyledForm>
-                <StyledFlex>
+                <StyledBlockFlex>
                     <StyledInput
+                        className="input"
                         type="text"
-                        placeholder="Անուն"
+                        placeholder={t("name")}
                         onChange={(e) => setData(prev => setNewData(prev, "name", e.target.value))}
                     />
                     <StyledInput
+                        className="input"
                         type="text"
-                        placeholder="Ազգնուն"
+                        placeholder={t("surname")}
                         onChange={(e) => setData(prev => setNewData(prev, "surname", e.target.value))}
                     />
-                </StyledFlex>
-                <StyledFlex>
+                </StyledBlockFlex>
+                <StyledBlockFlex>
                     <StyledInput
+                        className="input"
                         type="text"
-                        placeholder="Էլ. հասցե"
+                        placeholder={t("email")}
                         onChange={(e) => setData(prev => setNewData(prev, "email", e.target.value))}
                     />
                     <StyledInput
+                        className="input"
                         type="text"
-                        placeholder="Հեռախոսահամար"
+                        placeholder={t("telephone")}
                         onChange={(e) => setData(prev => setNewData(prev, "phone", e.target.value))}
                     />
-                </StyledFlex>
-                <StyledSubmitButton type="submit">
-                    Պատվիրել
-                </StyledSubmitButton>
+                </StyledBlockFlex>
+                {/* <StyledBlockFlex>
+                    <StyledSelect placeholder="Choose a service">
+                        <StyledOption>{t("narrow_specialist_advice")}</StyledOption>
+                        <option>{t("general_advice_consultation")}</option>
+                        <option>{t("prophylactic_examination")}</option>
+                    </StyledSelect>
+                </StyledBlockFlex> */}
+                <StyledBlockFlex>
+                    <StyledCustomSelect class="custom-select">
+                        <StyledSelected class="select-selected" onClick={() => setOpen(prev => !prev)}>
+                            {data?.service ?? "Select item"}
+                        </StyledSelected>
+                        {!!open && (<SelectItems class="select-items">
+                            {typeOfService.map(item => (
+                                <div
+                                    key={item}
+                                    onClick={() => {
+                                        setOpen(false);
+                                        setData(prev => setNewData(prev, "service", item))
+                                    }
+                                }>
+                                    {item}
+                                </div>
+                            ))}
+                        </SelectItems>)}
+                    </StyledCustomSelect>
+                </StyledBlockFlex> 
+                <StyledBlockFlex>
+                    <StyledTextarea className="input" placeholder={t("comment")} />
+                </StyledBlockFlex>
+                <StyledBlockFlex>
+                    <StyledSubmitButton type="submit">
+                        {t("book")}
+                    </StyledSubmitButton>
+                </StyledBlockFlex>
             </StyledForm>
         </StyledBookingForm>
     )
--- src/components/booking/bookingForm/styled.js
+++ src/components/booking/bookingForm/styled.js
@@ -14,15 +14,72 @@ export const StyledTitle = styled.h2`
 export const StyledForm = styled.form``
 
 export const StyledInput = styled.input`
-    background-color: #ccc;
+    background-color: #dedede;
     border-radius: 5px;
     border: none;
     padding: .5rem;
     margin: 5px;
     width: 100%;
+    &.input::placeholder {
+        color: #777;
+    }
     @media screen and (max-width: 550px) {
         margin: 0 0 16px 0;
     }
+`
+
+export const StyledSelect = styled.select`
+    background-color: #dedede;
+    border-radius: 5px;
+    border: none;
+    padding: .5rem;
+    margin: 5px;
+    width: 100%;
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+    cursor: pointer;
+    @media screen and (max-width: 550px) {
+        margin: 0 0 16px 0;
+    }
+`
+
+export const StyledCustomSelect = styled.div`
+    position: relative;
+    display: inline-block;
+    width: 100%;
+    margin: 5px;
+    text-align: left;
+`
+
+export const StyledSelected = styled.div`
+    background-color: #dedede;
+    color: #777;
+    padding: .5rem;
+    cursor: pointer;
+    border-radius: 5px;
+`
+
+export const SelectItems = styled.div`
+    position: absolute;
+    /* display: none; */
+    background-color: #d88181;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    border-radius: 4px;
+    z-index: 1;
+    width: 100%;
+    div {
+        padding: 10px;
+        cursor: pointer;
+    }
+    div:hover {
+        background-color: #c69c9c;
+    }
+`
+
+export const StyledOption = styled.option`
+    padding: 5px 0;
+    cursor: pointer;
 `
 
 export const StyledFlex = styled.div`
@@ -36,10 +93,28 @@ export const StyledFlex = styled.div`
     }
 `
 
+export const StyledBlockFlex = styled(StyledFlex)`
+    justify-content: space-between;
+`
+
+export const StyledTextarea = styled.textarea`
+    width: 100%;
+    background-color: #dedede;
+    border-radius: 5px;
+    border: none;
+    padding: .5rem;
+    margin: 5px;
+    resize: none;
+    &.input::placeholder {
+        color: #777;
+    }
+`
+
 export const StyledSubmitButton = styled.button`
     border-radius: 5px;
     width: 100%;
     border: none;
     background-color: #b3ffb3;
     padding: .5rem;
+    margin: 5px;
 `
