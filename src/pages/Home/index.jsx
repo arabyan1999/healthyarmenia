379455807b080693d554e18917238415b8764f4d
Davit2123 <arabyandavit777@gmail.com>
2024-11-24 19:26:09 +0400

    text change and one of pictures erased

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,7 +5,6 @@ import Feedback from "../../components/feedback";
 import { diseases, foods } from "../../data";
 import tiensProductLogo from "../../assets/tiens-product.jpeg";
 import t1 from "../../assets/t2.jpg";
-import t3 from "../../assets/TiensImage.webp";
 import t4 from "../../assets/tiens_nice_2_0.jpg";
 import Booking from "../../components/booking";
 import Foreword from "../../components/foreword";
@@ -18,7 +17,6 @@ function Home() {
         t1,
         tiensProductLogo,
         t4,
-        t3,
     ]
 
     const [loading, setLoading] = useState(true);
